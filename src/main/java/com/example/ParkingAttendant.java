3170f8f67df2e3989ee61be2c95aa669ec96b67a
Beast-20 <himanshuagrawal20042002@gmail.com>
2023-12-27 02:15:07 +0530

    Adding uc 10

--- src/main/java/com/example/ParkingAttendant.java
+++ src/main/java/com/example/ParkingAttendant.java
@@ -15,6 +15,25 @@ public class ParkingAttendant {
     }
 
     public void park_car(Car car) throws ParkinglotFullException{
+        if(car.get_handicap_status()){
+            int nearest = Integer.MAX_VALUE;
+            int taget_lot_number = 0;
+            for(int i = 0;i<list_of_parking_lot.size();i++){
+                Car[] curr_lot = list_of_parking_lot.get(i).get_lot();
+                for(int j = 0;j<curr_lot.length;j++){
+                    if(curr_lot[j]==null){
+                        if(j<nearest){
+                            nearest = j;
+                            taget_lot_number = i;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            list_of_parking_lot.get(taget_lot_number).park_car(car);
+        }
+        else{
         for(int i = 0;i<list_of_parking_lot.size();i++){
             if(!list_of_parking_lot.get(i).get_full_status()){
                 list_of_parking_lot.get(i).park_car(car);
@@ -24,6 +43,7 @@ public class ParkingAttendant {
                 throw new ParkinglotFullException("Parking lot is full!");
             }
         }
+    }
     }
 
     public double unpark_car(Car car) throws CarNotPresentException{
