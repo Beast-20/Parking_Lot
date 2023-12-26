55daee5c01c1a78baaf17005b738a172c0e2f5e1
Beast-20 <himanshuagrawal20042002@gmail.com>
2023-12-27 04:20:44 +0530

    Adding uc16

--- src/main/java/com/example/PoliceOfficer.java
+++ src/main/java/com/example/PoliceOfficer.java
@@ -68,4 +68,17 @@ public class PoliceOfficer {
         }
         return ans;
     }
+
+    public List<String> info_of_small_and_handicapped_cars(){
+        List<String> ans = new ArrayList<>();
+        for(int i = 0;i<lots.size();i++){
+            Car[] curr_lot = lots.get(i).get_lot();
+            for(int j = 0;j<curr_lot.length;j++){
+               if(curr_lot[j].get_type()=="Small" && curr_lot[j].get_handicap_status()==true){
+                 ans.add("Parking Lot:- "+i+", Position:- "+j+", "+"Car number:- "+curr_lot[j].get_number_string());
+               }
+            }
+        }
+        return ans;
+    }
 }
